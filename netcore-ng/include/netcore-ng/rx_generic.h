16a2e330688a30031cbde19533645a5c9e969e62
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-03-24 10:12:58 +0100

    Added missing header file.

---
+++ netcore-ng/include/netcore-ng/rx_generic.h
@@ -0,0 +1,43 @@
+/* __LICENSE_HEADER_BEGIN__ */
+
+/*
+ * Copyright (C) 2009, 2011  Daniel Borkmann <daniel@netsniff-ng.org> and
+ *                           Emmanuel Roullit <emmanuel@netsniff-ng.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ */
+
+ /* __LICENSE_HEADER_END__ */
+
+#ifndef __NET_RX_GENERIC_H__
+#define __NET_RX_GENERIC_H__
+
+#include <net/if.h>
+
+#include <netcore-ng/rx_job.h>
+#include <netcore-ng/bpf.h>
+
+struct rx_generic_nic_context
+{
+	/* Structure which describe a nic instead? */
+	char 					rx_dev[IFNAMSIZ];
+	int					dev_fd;
+	int 					pcap_fd;
+	struct sock_fprog 			bpf;
+	struct rx_job_list			job_list;
+};
+
+#endif /* __NET_RX_GENERIC_H__ */
