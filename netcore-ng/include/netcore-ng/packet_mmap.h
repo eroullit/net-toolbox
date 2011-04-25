c73d1f7ba6a259bf597642337657813f705f080d
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-04-25 11:38:23 +0200

    Added packet mmap context function to handle both RX/TX ring.

---
+++ netcore-ng/include/netcore-ng/packet_mmap.h
@@ -0,0 +1,30 @@
+#ifndef __PACKET_MMAP_H__
+#define __PACKET_MMAP_H__
+
+#include <stdint.h>
+#include <linux/if_packet.h>
+
+enum packet_mmap_ctx_type
+{
+	PACKET_MMAP_RX = PACKET_RX_RING,
+	PACKET_MMAP_TX = PACKET_TX_RING
+};
+
+struct packet_mmap_ctx
+{
+	enum tpacket_versions 		version;
+	enum packet_mmap_ctx_type 	type;
+	int 				sock;
+	int 				ifindex;
+	struct tpacket_req		layout;
+	struct iovec *			mmap_vec;
+	uint8_t *			mmap_buf;
+};
+
+struct packet_mmap_header 
+{
+	struct tpacket_hdr tp_h __attribute__ ((aligned(TPACKET_ALIGNMENT)));
+	struct sockaddr_ll s_ll __attribute__ ((aligned(TPACKET_ALIGNMENT)));
+};
+
+#endif /* __PACKET_MMAP_H__ */
---
+++ netcore-ng/packet_mmap.c
@@ -0,0 +1,159 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+
+#include <netcore-ng/macros.h>
+#include <netcore-ng/packet_mmap.h>
+
+static int packet_mmap_ctx_register(const struct packet_mmap_ctx * const pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+
+	if (setsockopt(pkt_mmap_ctx->sock, SOL_PACKET, pkt_mmap_ctx->type, (void *)(&pkt_mmap_ctx->layout), sizeof(pkt_mmap_ctx->layout)) < 0) {
+		err("Could not register packet mmap context");
+		return (EAGAIN);
+	}
+
+	return (0);
+}
+
+static void packet_mmap_ctx_unregister(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+
+	memset(&pkt_mmap_ctx->layout, 0, sizeof(pkt_mmap_ctx->layout));
+
+	setsockopt(pkt_mmap_ctx->sock, SOL_PACKET, pkt_mmap_ctx->type, (void *)(&pkt_mmap_ctx->layout), sizeof(pkt_mmap_ctx->layout));
+}
+
+static int packet_mmap_ctx_mmap(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+
+	pkt_mmap_ctx->mmap_buf = mmap(0, pkt_mmap_ctx->layout.tp_block_size * pkt_mmap_ctx->layout.tp_block_nr, PROT_READ | PROT_WRITE, MAP_SHARED, pkt_mmap_ctx->sock, 0);
+
+	if (pkt_mmap_ctx->mmap_buf == MAP_FAILED)
+	{
+		err("Could not mmap the packet mmap context");
+		return (EINVAL);
+	}
+
+	return (0);
+}
+
+static void packet_mmap_ctx_munmap(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+
+	if (pkt_mmap_ctx->mmap_buf)
+	{
+		munmap(pkt_mmap_ctx->mmap_buf, pkt_mmap_ctx->layout.tp_block_size * pkt_mmap_ctx->layout.tp_block_nr);
+		pkt_mmap_ctx->mmap_buf = NULL;
+	}
+}
+
+static int packet_mmap_ctx_bind(const struct packet_mmap_ctx * const pkt_mmap_ctx)
+{
+	struct sockaddr_ll sll;
+	
+	assert(pkt_mmap_ctx);
+
+	memset(&sll, 0, sizeof(sll));
+	sll.sll_family = AF_PACKET;
+	sll.sll_protocol = htons(ETH_P_ALL);
+	sll.sll_ifindex = pkt_mmap_ctx->ifindex;
+
+	if (bind(pkt_mmap_ctx->sock, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
+		err("Could not bind packet mmap context to device");
+		return (EINVAL);
+	}
+
+	/* Check error and if dev is ready */
+
+	return (0);
+}
+
+static int packet_mmap_ctx_vector_create(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	size_t a;
+
+	assert(pkt_mmap_ctx);
+	assert(pkt_mmap_ctx->mmap_buf);
+
+	pkt_mmap_ctx->mmap_vec = calloc(pkt_mmap_ctx->layout.tp_frame_nr, sizeof(*pkt_mmap_ctx->mmap_vec));
+
+	if (!pkt_mmap_ctx->mmap_vec)
+	{
+		err("Could not allocate packet mmap I/O vectors");
+		return (ENOMEM);
+	}
+
+	for (a = 0; a < pkt_mmap_ctx->layout.tp_frame_nr; a++)
+	{
+		pkt_mmap_ctx->mmap_vec[a].iov_base = &pkt_mmap_ctx->mmap_buf[a * pkt_mmap_ctx->layout.tp_frame_size];
+		pkt_mmap_ctx->mmap_vec[a].iov_len = pkt_mmap_ctx->layout.tp_frame_size;
+	}
+
+	return (0);
+}
+
+static void packet_mmap_ctx_vector_destroy(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+	free(pkt_mmap_ctx->mmap_vec);
+}
+
+void packet_mmap_ctx_destroy(struct packet_mmap_ctx * pkt_mmap_ctx)
+{
+	assert(pkt_mmap_ctx);
+
+	packet_mmap_ctx_vector_destroy(pkt_mmap_ctx);
+	packet_mmap_ctx_munmap(pkt_mmap_ctx);
+	packet_mmap_ctx_unregister(pkt_mmap_ctx);
+
+	memset(pkt_mmap_ctx, 0, sizeof(*pkt_mmap_ctx));
+}
+
+int packet_mmap_ctx_create(struct packet_mmap_ctx * pkt_mmap_ctx, const struct tpacket_req const * req, const int ifindex, const int sock, const enum packet_mmap_ctx_type type)
+{
+	int rc = 0;
+
+	assert(pkt_mmap_ctx);
+	assert(req);
+
+	memset(pkt_mmap_ctx, 0, sizeof(*pkt_mmap_ctx));
+	pkt_mmap_ctx->type = type;
+	pkt_mmap_ctx->layout = *req;
+	pkt_mmap_ctx->sock = sock;
+	pkt_mmap_ctx->ifindex = ifindex;
+
+	if ((rc = packet_mmap_ctx_register(pkt_mmap_ctx)) != 0)
+	{
+		goto out;
+	}
+
+	if ((rc = packet_mmap_ctx_mmap(pkt_mmap_ctx)) != 0)
+	{
+		goto out;
+	}
+
+	if ((rc = packet_mmap_ctx_vector_create(pkt_mmap_ctx)) != 0)
+	{
+		goto out;
+	}
+
+	if ((rc = packet_mmap_ctx_bind(pkt_mmap_ctx)) != 0)
+	{
+		goto out;
+	}
+
+	return (0);
+out:
+	packet_mmap_ctx_destroy(pkt_mmap_ctx);
+	return (rc);
+}
