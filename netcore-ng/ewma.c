5e1108aecae786fc232de2536c3c6af89fd43265
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-06-20 17:50:59 +0200

    Added EWMA function to use in statistics.

---
+++ netcore-ng/ewma.c
@@ -0,0 +1,90 @@
+/**
+ * \file ewma.c
+ * \author written by Bruno Randolf <br1@einfach.org> (c)
+ * \date 2011
+ */
+
+/* __LICENSE_HEADER_BEGIN__ */
+
+/*
+ * Copyright (C) 2009-2011	Emmanuel Roullit <emmanuel@netsniff-ng.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ */
+
+ /* __LICENSE_HEADER_END__ */
+
+#include <assert.h>
+#include <math.h>
+#include <netcore-ng/ewma.h>
+
+/**
+ * DOC: Exponentially Weighted Moving Average (EWMA)
+ *
+ * These are generic functions for calculating Exponentially Weighted Moving
+ * Averages (EWMA). We keep a structure with the EWMA parameters and a scaled
+ * up internal representation of the average value to prevent rounding errors.
+ * The factor for scaling up and the exponential weight (or decay rate) have to
+ * be specified thru the init fuction. The structure should not be accessed
+ * directly but only thru the helper functions.
+ */
+
+/**
+ * \brief Tell if a integer is a power of 2
+ * \param[in] n Integer to test
+ * \return 1 if true, 0 otherwise
+ */
+
+static inline int is_power_of_2 (uint64_t n)
+{
+	return (n != 0 && ((n & (n - 1)) == 0));
+}
+
+/**
+ * \brief Initialize EWMA parameters
+ * \param[in,out] avg Average structure
+ * \param[in] factor Factor to use for the scaled up internal value. 
+ *	The maximum value of averages can be ULONG_MAX/(factor*weight).
+ *	For performance reasons factor has to be a power of 2.
+ * \param[in] weight Exponential weight, or decay rate.
+ *	This defines how fast the influence of older values decreases.
+ *	For performance reasons weight has to be a power of 2.
+ */
+
+void ewma_init(struct ewma *avg, uint64_t factor, uint64_t weight)
+{
+	assert(is_power_of_2(weight));
+	assert(is_power_of_2(factor));
+
+	avg->weight = log2(weight);
+	avg->factor = log2(factor);
+	avg->internal = 0;
+}
+
+/**
+ * \brief Exponentially weighted moving average (EWMA)
+ * \param[in,out] avg Average structure
+ * \param[in] val Current value
+ */
+struct ewma *ewma_add(struct ewma *avg, uint64_t val)
+{
+	avg->internal = avg->internal  ?
+		(((avg->internal << avg->weight) - avg->internal) +
+			(val << avg->factor)) >> avg->weight :
+		(val << avg->factor);
+	
+	return avg;
+}
---
+++ netcore-ng/include/netcore-ng/ewma.h
@@ -0,0 +1,56 @@
+/**
+ * \file ewma.h
+ * \author written by Bruno Randolf <br1@einfach.org> (c)
+ * \date 2011
+ */
+
+/* __LICENSE_HEADER_BEGIN__ */
+
+/*
+ * Copyright (C) 2010-2011	Emmanuel Roullit <emmanuel@netsniff-ng.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ */
+
+ /* __LICENSE_HEADER_END__ */
+
+#ifndef EWMA_H
+#define	EWMA_H
+
+#include <stdint.h>
+
+struct ewma {
+	uint64_t internal;
+	uint64_t factor;
+	uint64_t weight;
+};
+
+void ewma_init(struct ewma *avg, uint64_t factor, uint64_t weight);
+struct ewma *ewma_add(struct ewma *avg, uint64_t val);
+
+/**
+ * \brief Read average struct
+ * \param[in,out] avg Average structure
+ * \return average value
+ */
+
+static inline uint64_t ewma_read(const struct ewma *avg)
+{
+	return avg->internal >> avg->factor;
+}
+
+#endif	/* EWMA_H */
+
